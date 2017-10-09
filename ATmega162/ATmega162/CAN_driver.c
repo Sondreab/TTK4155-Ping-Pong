3be13feff3153be3ed4648fc4fd428a433efe714
Sondreab <sondre.bergum@gmail.com>
2017-10-09 15:54:54 +0200

    Added some minor notes in the CAN_init function.

--- ATmega162/ATmega162/CAN_driver.c
+++ ATmega162/ATmega162/CAN_driver.c
@@ -27,6 +27,14 @@ uint8_t CAN_init() {
 	
 	//TODO: the rest of init
 	
+	//11 or 29 bit identifier? Register 3-2
+	
+	
 	
 	return 0;
 }
+
+//NOTES:
+//the MAB register will overwrite everything in the recieve buffer when a new message is received that passes the acceptance criteria, 
+//the MCU must clear the CANINTF.RXnIF bit to signify that it's "done" with a message.
+//Rollover? 4.2.1
